132fa2f790f61d8fc9c64132fea1bafe52fe5374
_rcH <airyktawiah@gmail.com>
2023-11-01 20:05:10 -0400

    New Push

---
+++ src/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+  {
+    name: 'Free',
+    slug: 'free',
+    quota: 500,
+    pagesPerPdf: 1000,
+    price: {
+      amount: 0,
+      priceIds: {
+        test: '',
+        production: '',
+      },
+    },
+  },
+  {
+    name: 'Pro',
+    slug: 'pro',
+    quota: 500,
+    pagesPerPdf: 1000,
+    price: {
+      amount: 14,
+      priceIds: {
+        test: 'price_1NuEwTA19umTXGu8MeS3hN8L',
+        production: '',
+      },
+    },
+  },
+]
